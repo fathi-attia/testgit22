b5d0aa27dd121a06350ac0ba7b7da6e3ea48f388
fathi <fathy.attia@hotmail.com>
2025-02-02 16:58:46 +0400

    b8

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello, World!b667from stg");
+            Console.WriteLine("Hello, World!b6678from stg");
 
     }
 }
